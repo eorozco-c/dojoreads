3301ea68747425a4b5568bc990ccac08047649a6
Ernesto Orozco <42546849+eorozco-c@users.noreply.github.com>
2021-08-19 19:27:34 -0400

    Update settings.py

--- dojoreads/settings.py
+++ dojoreads/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y^3#m38du7hf@b%f@mb1)i363-q+j4om1pz76=vo32(=9ote#6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost","52.14.4.199","54.172.175.195"]
 
 
 # Application definition
@@ -124,3 +124,5 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
